99f3e43ae215d5f7fdfa604d998bb7dffbf8e2c8
raghav <raghav_sobti@yahoo.com>
2024-10-14 10:07:32 +0400

    enabled cors for all origins
    enabled cors for all origins

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,11 +7,21 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  // app.enableCors({
+  //   origin: 'http://localhost:5173',
+  //   credentials: true,
+  // });
+
+  // Enable CORS for all origins
   app.enableCors({
-    // origin: 'http://localhost:5173',
-    origin: '*',
-    credentials: true,
+    // origin: ['*'], // Allows all origins
+    origin: (origin, callback) => {
+      callback(null, true); // Allow all origins
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials if needed (optional)
   });
+
   await app.listen(3000);
 }
 bootstrap();
