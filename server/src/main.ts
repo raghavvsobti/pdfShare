8e362408351d425451a998c99be3d5670487269a
raghav <raghav_sobti@yahoo.com>
2024-10-14 19:44:49 +0400

    cleanup
    cleanup

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,19 +7,13 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  // app.enableCors({
-  //   origin: 'http://localhost:5173',
-  //   credentials: true,
-  // });
 
-  // Enable CORS for all origins
   app.enableCors({
-    // origin: ['*'], // Allows all origins
-    origin: (origin, callback) => {
+    origin: (_, callback) => {
       callback(null, true); // Allow all origins
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow credentials if needed (optional)
+    credentials: true,
   });
 
   await app.listen(3000);
